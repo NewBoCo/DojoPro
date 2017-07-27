e703fbc1cc80b5365aad023dd7e90dfc2a1bee45
jalane <jesse.a.lane@gmail.com>
2017-07-27 12:08:35 -0500

    Adding the code so students can recover if they bork they're own project.

---
+++ Summer Challenge 2017/bnb/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+    public Rigidbody ball;
+    public Transform throwPosition;
+    public float throwForce = 1000f;
+    	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody thrownBall = Instantiate(ball, throwPosition.position, throwPosition.rotation);
+            thrownBall.AddForce(throwPosition.forward * throwForce);
+        }
+	}
+}
